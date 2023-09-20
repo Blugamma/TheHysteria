e6267d43f455b0a6530b0844b522846681cee1be
Blugamma <blugammadesign@gmail.com>
2023-09-20 23:54:16 +0100

    added D1

--- functions/main.js
+++ functions/main.js
@@ -1,3 +1,11 @@
 export function onRequest(context) {
     return new Response("Hello, world!")
   }
+
+  export async function onRequest(context) {
+    // Create a prepared statement with our query
+    const ps = context.env.TheHysteria.prepare('SELECT * from players');
+    const data = await ps.first();
+  
+    return Response.json(data);
+  }
