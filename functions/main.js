edd3d94d31cc092b101e4dfba57077855b53498e
Blugamma <blugammadesign@gmail.com>
2023-09-20 23:54:52 +0100

    updated D1

--- functions/main.js
+++ functions/main.js
@@ -1,6 +1,4 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
-  }
+
 
   export async function onRequest(context) {
     // Create a prepared statement with our query
