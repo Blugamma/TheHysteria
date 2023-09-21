fa29ea828d14e6ded978611dc05288c6b66b8bc5
Blugamma <blugammadesign@gmail.com>
2023-09-21 18:34:41 +0100

    Added edge functions

--- functions/main.js
+++
@@ -1,9 +0,0 @@
-
-
-  export async function onRequest(context) {
-    // Create a prepared statement with our query
-    const ps = context.env.TheHysteria.prepare('SELECT * from players');
-    const data = await ps.first();
-  
-    return Response.json(data);
-  }
